80b289fdf6834a949d8b4cebf789fbea99bb6e60
pangyongsheng <pang_yongsheng@outlook.com>
2018-09-04 15:43:46 +0800

    add session

--- controllers/signin.js
+++ controllers/signin.js
@@ -7,18 +7,24 @@ module.exports = {
 
         if (email === '123@163.com' && password === '123456') { //判断是否正确
             //登录成功
-            //设置cookie
-            ctx.cookies.set(
-                'cid',
-                'hello world', {
-                    domain: 'localhost', //写cookie所在的域名
-                    path: '/', // 写cookie所在的路径
-                    maxAge: 10 * 60 * 1000, // cookie有效时长
-                    expires: new Date('2018-09-1'), // cookie失效时间
-                    httpOnly: false, // 是否只用于http请求中获取
-                    overwrite: false // 是否允许重写
-                }
-            )
+            //设置cookie////////////////////
+            // ctx.cookies.set(
+            //     'cid',
+            //     'hello world', {
+            //         domain: 'localhost', //写cookie所在的域名
+            //         path: '/', // 写cookie所在的路径
+            //         maxAge: 10 * 60 * 1000, // cookie有效时长
+            //         expires: new Date('2018-09-1'), // cookie失效时间
+            //         httpOnly: false, // 是否只用于http请求中获取
+            //         overwrite: false // 是否允许重写
+            //     }
+            // )
+            //////////////////////////////////
+            //设置session
+            ctx.session.user = {
+                email,
+                password
+            }
             ctx.render('signin-ok.html', {
                 title: '登录成功',
                 name: email
--- session.js
+++ session.js
@@ -1,5 +1,13 @@
 const session = require('koa-session-minimal');
 
-module.export=app => {
 
-}
+module.exports = session({
+    key: 'session-id', // cookie 中存储 session-id 时的键名, 默认为 koa:sess
+    cookie: { // 与 cookie 相关的配置
+        domain: 'localhost', // 写 cookie 所在的域名
+        path: '/', // 写 cookie 所在的路径
+        maxAge: 1000 * 30, // cookie 有效时长
+        httpOnly: true, // 是否只用于 http 请求中获取
+        overwrite: false // 是否允许重写
+    }
+})
